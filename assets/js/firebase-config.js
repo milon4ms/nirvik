1b4b13c476a89df1dd98f0bf3838621a1c391323
Md. Ashaduzzaman <milon4ms@gmail.com>
2026-06-20 21:43:20 +0600

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,13 +1,15 @@
 // assets/js/firebase-config.js
 // এখানে কোনো import/export নেই – শুধু সাধারণ JS
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSy...",          // আপনার আসল API Key
-    authDomain: "nirvik-72943.firebaseapp.com",
-    projectId: "nirvik-72943",
-    storageBucket: "nirvik-72943.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDy0r1tnJV09d7wgLplEqMsRz45_0p37CA",
+  authDomain: "nirvik-72943.firebaseapp.com",
+  projectId: "nirvik-72943",
+  storageBucket: "nirvik-72943.firebasestorage.app",
+  messagingSenderId: "648899156613",
+  appId: "1:648899156613:web:798ed1758e076e8a62a7ec",
+  measurementId: "G-DQ32VPXH3Y"
 };
 
 // Firebase Initialize (global firebase object)
